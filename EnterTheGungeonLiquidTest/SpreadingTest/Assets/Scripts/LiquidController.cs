09954567ce2530c0e432b22a359304260e1c41f0
Arnau <arnau.aguilar@enti.cat>
2021-04-03 19:35:12 +0200

    shaders and stuff

--- EnterTheGungeonLiquidTest/SpreadingTest/Assets/Scripts/LiquidController.cs
+++ EnterTheGungeonLiquidTest/SpreadingTest/Assets/Scripts/LiquidController.cs
@@ -23,6 +23,7 @@ public class LiquidController : MonoBehaviour
 
     public float diffuseSpeed = 1;
     public float expandSpeed = 1;
+    public float dieSpeed = 1;
     public float stopSpreadingLifetime;
 
     void Start()
@@ -81,6 +82,7 @@ public class LiquidController : MonoBehaviour
     {
         computeShader.SetFloat("dt", Time.deltaTime);
         computeShader.SetFloat("expandSpeed", expandSpeed);
+        computeShader.SetFloat("dieSpeed", dieSpeed);
         computeShader.SetFloat("diffuseSpeed", diffuseSpeed);
         computeShader.SetFloat("stopSpreadingLifetime", stopSpreadingLifetime);
 

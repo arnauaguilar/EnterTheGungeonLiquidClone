9953b6163ad3764adc428268bf6f90ad1b9ae898
Arnau <arnau.aguilar@enti.cat>
2021-04-04 12:53:22 +0200

    Areas working

--- EnterTheGungeonLiquidTest/SpreadingTest/Assets/Scripts/SphereUpdater.cs
+++ EnterTheGungeonLiquidTest/SpreadingTest/Assets/Scripts/SphereUpdater.cs
@@ -6,23 +6,30 @@ public class SphereUpdater : MonoBehaviour
 {
     private Camera cam;
     private Renderer renderer;
+    private Renderer areaRenderer;
     void Start()
     {
         cam = Camera.main;
         renderer = GetComponent<Renderer>();
+        areaRenderer = transform.GetChild(0).GetComponent<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 mousePos = Input.mousePosition;
-        transform.position = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 5));
+        Vector3 pos = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 0));
+        transform.position = new Vector3(pos.x, pos.y, 10);
 
         if (Input.GetMouseButton(0))
+        {
             renderer.enabled = true;
+            areaRenderer.enabled = true;
+        }
         else
         {
             renderer.enabled = false;
+            areaRenderer.enabled = false;
         }
 
     }
